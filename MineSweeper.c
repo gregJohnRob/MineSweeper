71772a82ad101ecc96337d4f24380c5091343cb1
2076831r <gregor_r@btinternet.com>
2015-05-17 00:28:58 +0100

    program now uses pointers and structs for the mine field and users can now specify number of mines and size of the mine field

--- MineSweeper.c
+++ MineSweeper.c
@@ -3,41 +3,56 @@
  * TODO: allow the user to specify a size and number of mines: will look at pointers and 2D arrays
  * TODO: add in error checking (for example make sure the user types in two numbers for a point
  * TODO: Make code neater
+ * TODO: change code to be implemented using pointers
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <ncurses.h>
-
-// you can change the size of the mine field and the number of mines
-// note however the higher the mine to free space ratio, the slower populate will run
-// also make sure that the xDim and yDim match the dimensions of mine and displayMine
-static int xDim = 16;
-static int yDim = 16;
-static int numberOfMines = 100;
-static int mine[16][16] = {0};
-static int mineValue = 100;
-static int displayMine[16][16] = {0};
+typedef struct {
+  int value;
+  int displayValue;
+} Position;
 
 
-void populate(){
+Position** createMineField(int xDimension, int yDimension) {
+  Position** mineField;
+  mineField = (Position**) malloc(xDimension*sizeof(Position*));
+  int i;
+  int j;
+  for (i = 0; i < xDimension; i++) {
+    mineField[i] = (Position*) malloc(yDimension*sizeof(Position));
+    for (j = 0; j < yDimension; j++) {
+      (mineField[i] + j)->value = 0;
+      (mineField[i] + j)->displayValue = 0;
+    }
+  }
+  return mineField;
+}
+/*
+Summary: If you have a multidimensional array defined as int [][], then x = y[a][b] is equivalent to x = *((int *)y + a * NUMBER_OF_COLUMNS + b);
+*/
+void populate(Position** mineField,
+              int xDimension, int yDimension,
+              int numberOfMines, int mineValue) {
+
   srand(time(NULL));
-  int x = rand() % xDim;
-  int y = rand() % yDim;
+  int x = rand() % xDimension;
+  int y = rand() % yDimension;
   int i;
   for (i = 0; i < numberOfMines; i++){
-    while(mine[x][y] == mineValue){
-      x = rand() % xDim;
-      y = rand() % yDim;
+    while((mineField[x] + y)->value == mineValue){
+      x = rand() % xDimension;
+      y = rand() % yDimension;
     }
-    mine[x][y] = mineValue;
+    (mineField[x] + y)->value = mineValue;
     int xtemp;
     int ytemp;
-    for (xtemp = x - 1; xtemp < x + 2 && xtemp < xDim; xtemp++) {
-      for (ytemp = y - 1; xtemp >= 0 && ytemp < y + 2 && ytemp < yDim; ytemp++) {
-        if (ytemp >= 0 && mine[xtemp][ytemp] != mineValue) {
-          mine[xtemp][ytemp]++;
+    for (xtemp = x - 1; xtemp < x + 2 && xtemp < xDimension; xtemp++) {
+      for (ytemp = y - 1; xtemp >= 0 && ytemp < y + 2 && ytemp < yDimension; ytemp++) {
+        if (ytemp >= 0 && (mineField[xtemp] + ytemp)->value != mineValue) {
+          (mineField[xtemp] + ytemp)->value += 1;
         }
       }
     }
@@ -46,35 +61,36 @@ void populate(){
 
 
 // the display function that can be used if you want to test out different things
-void displayForTesting(){
+void displayForTesting(Position** mineField,
+              int xDimension, int yDimension, int mineValue){
   int x;
   int y;
   int largestValue = 0;
   printw("      ");
-  for (x = 0; x < xDim; x++) {
+  for (x = 0; x < xDimension; x++) {
     if (x < 10) {
       printw("00%d ", x);
     } else {
       printw("0%d ", x);
     }
   }
-  printw("\n \n");
-  for (y = 0; y < yDim; y++) {
+  printw("\n\n");
+  for (y = 0; y < yDimension; y++) {
     if (y < 10) {
       printw("00%d   ", y);
     } else {
       printw("0%d   ", y);
     }
-    for (x = 0; x < xDim; x++) {
-      if (mine[x][y] > largestValue && mine[x][y] != mineValue){
-        largestValue = mine[x][y];
+    for (x = 0; x < xDimension; x++) {
+      if ((mineField[x] + y)->value > largestValue && (mineField[x] + y)->value != mineValue){
+        largestValue = (mineField[x] + y)->value;
       }
-      if (mine[x][y] < 10) {
-        printw("00%d ", mine[x][y]);
-      } else if (mine[x][y] < 100){
-        printw("0%d ", mine[x][y]);
+      if ((mineField[x] + y)->value < 10) {
+        printw("00%d ", (mineField[x] + y)->value);
+      } else if ((mineField[x] + y)->value < 100){
+        printw("0%d ", (mineField[x] + y)->value);
       } else {
-        printw("%d ", mine[x][y]);
+        printw("%d ", (mineField[x] + y)->value);
       }
     }
     printw("\n");
@@ -95,11 +111,12 @@ void displayLastMove(int x, char point[]){
   }
 }
 
-void display(){
+void display(Position** mineField,
+              int xDimension, int yDimension, int mineValue){
   int x;
   int y;
   printw("      ");
-  for (x = 0; x < xDim; x++) {
+  for (x = 0; x < xDimension; x++) {
     if (x < 10) {
       printw("00%d ", x);
     } else {
@@ -107,22 +124,22 @@ void display(){
     }
   }
   printw("\n \n");
-  for (y = 0; y < yDim; y++) {
+  for (y = 0; y < yDimension; y++) {
     if (y < 10) {
       printw("00%d   ", y);
     } else {
       printw("0%d   ", y);
     }
-    for (x = 0; x < xDim; x++) {
-      if (displayMine[x][y] == 0){
+    for (x = 0; x < xDimension; x++) {
+      if ((mineField[x] + y)->displayValue == 0){
         printw("??? ");
-      } else if (displayMine[x][y] == 1) {
-        if (mine[x][y] < 10) {
-          printw("00%d ", mine[x][y]);
-        } else if (mine[x][y] < 100){
-          printw("0%d ", mine[x][y]);
+      } else if ((mineField[x] + y)->displayValue == 1) {
+        if ((mineField[x] + y)->value < 10) {
+          printw("00%d ", (mineField[x] + y)->value);
+        } else if ((mineField[x] + y)->value < 100){
+          printw("0%d ", (mineField[x] + y)->value);
         }
-      } else if (displayMine[x][y] == 2) {
+      } else if ((mineField[x] + y)->displayValue == 2) {
         printw("fla ");
       }
     }
@@ -130,32 +147,32 @@ void display(){
   }
 }
 
-void show(int x, int y) {
-  if (x < 0 || y < 0 || x >= xDim || y >= yDim || mine[x][y] == mineValue || displayMine[x][y] != 0) {
+void show(int x, int y, int xDimension, int yDimension, Position** mineField) {
+  if (x < 0 || y < 0 || x >= xDimension || y >= yDimension || (mineField[x] + y)->displayValue != 0) {
     return;
-  } else if (mine[x][y] != 0) {
-    displayMine[x][y] = 1;
+  } else if ((mineField[x] + y)->value != 0) {
+    (mineField[x] + y)->displayValue = 1;
     return;
   } else {
-    displayMine[x][y] = 1;
+    (mineField[x] + y)->displayValue = 1;
     int i;
     int j;
     for (i = x - 1; i < x + 2; i++) {
       for (j = y - 1; j < y + 2; j++) {
-        show(i, j);
+        show(i, j, xDimension, yDimension, mineField);
       }
     }
   }
 }
 
-int check(){
+int check(Position** mineField, int xDimension, int yDimension, int mineValue){
   int x;
   int y;
-  for(x = 0; x < xDim; x++) {
-    for (y = 0; y < yDim; y++){
-      if (displayMine[x][y] == 0) {
+  for(x = 0; x < xDimension; x++) {
+    for (y = 0; y < yDimension; y++){
+      if ((mineField[x] + y)->displayValue == 0) {
         return 1;
-      } else if (displayMine[x][y] == 2 && mine[x][y] != mineValue) {
+      } else if ((mineField[x] + y)->displayValue == 2 && (mineField[x] + y)->value != mineValue) {
         return 1;
       }
     }
@@ -164,19 +181,32 @@ int check(){
 }
 
 int main(void) {
-  initscr();
-  populate();
+  int mineValue = 100;
+  int xDimension;
+  int yDimension;
+  int numberOfMines;
   int x;
   int y;
-  char command[10];
+  char command[20];
   int  lastCommand = 0;
 
+  initscr();
+  printw("Enter the size of the mine field. Enter as: X Y \n");
+  refresh();
+  getstr(command);
+  sscanf(command, "%d %d", &xDimension, &yDimension);
+  Position** mineField = createMineField(xDimension, yDimension);
+  printw("How many mines would you like? ");
+  refresh();
+  getstr(command);
+  sscanf(command, "%d", &numberOfMines);
+  populate(mineField, xDimension, yDimension, numberOfMines, mineValue);
   while(1==1){
     clear();
     printw("commands: \n l: check a location \n e: exit the game \n f: flag a location\n");
     printw("When writing coordinates write as 'x y' where x and y are the x and y coordinates and %s",
               "are positive and less than 16\n");
-    display();
+    display(mineField, xDimension, yDimension, mineValue);
     displayLastMove(lastCommand, command);
     printw("What would you like to do?\n ");
     refresh();
@@ -192,20 +222,19 @@ int main(void) {
       refresh();
       getstr(command);
       sscanf(command, "%d %d", &x, &y);
-      if (mine[x][y] == mineValue) {
+      if ((mineField[x] + y)->value == mineValue && (mineField[x] + y)->displayValue != 2) {
         printw("You are dead\nWould you like to restart? Y/N\n");
         refresh();
         getstr(command);
         if (strncmp(command, "Y", 1) == 0) {
-          memset(displayMine, 0, sizeof(displayMine));
-          memset(mine, 0, sizeof(mine));
-          populate();
+          mineField = createMineField(xDimension, yDimension);
+          populate(mineField, xDimension, yDimension, numberOfMines, mineValue);
           lastCommand = 0;
         } else {
           break;
         }
       } else {
-        show(x, y);
+        show(x, y, xDimension, yDimension, mineField);
       }
     } else if (strncmp(command, "f", 1) == 0) {
       lastCommand = 2;
@@ -213,13 +242,13 @@ int main(void) {
       getstr(command);
       refresh();
       sscanf(command, "%d %d", &x, &y);
-      if (displayMine[x][y] == 0) {
-        displayMine[x][y] = 2;
-      } else if (displayMine[x][y] == 2) {
-        displayMine[x][y] = 0;
+      if ((mineField[x] + y)->displayValue == 0) {
+        (mineField[x] + y)->displayValue = 2;
+      } else if ((mineField[x] + y)->displayValue == 2) {
+        (mineField[x] + y)->displayValue = 0;
       }
     }
-    if (check() == 0) {
+    if (check(mineField, xDimension, yDimension, mineValue) == 0) {
       printw("You win!\n");
       refresh();
       break;
